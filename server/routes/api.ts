ac358e7653e3aff80868260f364738b06f437590
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2018-03-21 12:06:12 +0200

    remove tmp person token later on so that log won't have the real person token

--- server/routes/api.ts
+++ server/routes/api.ts
@@ -12,8 +12,7 @@ let logger: any;
 apiRouter.all('*', isAuthenticated, (req: any, res: Response) => {
   // convert public query to private
   const url = req.url
-    .replace('/query/', '/private-query/')
-    .replace('_personToken_', req.user['token']);
+    .replace('/query/', '/private-query/');
 
   // logout also from the backend when logging out
   if (url.indexOf('/person-token/') === 0 && req.method === 'DELETE') {
@@ -28,7 +27,7 @@ apiRouter.all('*', isAuthenticated, (req: any, res: Response) => {
     remote: req.connection.remoteAddress || ''
   });
   req.pipe(httpRequest(
-    apiUrl + url,
+    apiUrl + url.replace('_personToken_', req.user['token']),
     {
       headers: {
         'accept': 'application/json',
