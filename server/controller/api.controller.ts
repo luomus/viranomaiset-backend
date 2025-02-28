6b5b0ebb61254b2fe23f9d419a0045973d40f047
aorin <meeri.rannisto@gmail.com>
2025-02-28 16:05:13 +0200

    small refactoring

--- server/controller/api.controller.ts
+++ server/controller/api.controller.ts
@@ -24,60 +24,11 @@ export class ApiController {
     // change the body to have real token if it's there
     const body = replacePublicTokenInBody(req.body, req.user.publicToken, req.user['token']);
 
-    function doRemoteRequest(user: string, url: string, body, req: Request, res: Response<any>) {
-      const start = Date.now();
-      const apiTarget = new URL(apiUrl + url);
-
-      let request;
-      if (url.includes('/geo-convert')) {
-        request = req.pipe(httpRequest(
-          apiTarget.toString(),
-          {
-            headers: {
-              ...req.headers,
-              'Host': apiTarget.hostname,
-              'authorization': accessToken
-            }
-          }
-        ));
-      } else {
-        request = httpRequest[req.method.toLowerCase()](
-          apiTarget.toString(),
-          {
-            headers: {
-              ...req.headers,
-              'Host': apiTarget.hostname,
-              'content-length': typeof body === 'string' ? Buffer.byteLength(body) : req.headers['content-length'],
-              'authorization': accessToken
-            },
-            ...(['GET', 'DELETE'].includes(req.method) ? {} : {body})
-          }
-        );
-      }
-
-      request.on('end', function () {
-        LoggerService.info({
-          user,
-          action: LOG_SUCCESS,
-          request: {
-            method: req.method,
-            url,
-            body
-          },
-          response: {
-            statusCode: res.statusCode,
-          },
-          took: (Date.now() - start),
-          remote: req.connection.remoteAddress || '',
-        });
-      }).pipe(res);
-    }
-
     // logout also from the backend when logging out
     if (url.indexOf('/person-token/') === 0 && req.method === 'DELETE') {
-      req.logout(function () {
+      req.logout(() => {
         req.session.destroy(() => {
-          doRemoteRequest(user, url, body, req, res);
+          this.doRemoteRequest(user, url, body, req, res);
         });
         LoggerService.info({
           user,
@@ -85,7 +36,56 @@ export class ApiController {
         });
       });
     } else {
-      doRemoteRequest(user, url, body, req, res);
+      this.doRemoteRequest(user, url, body, req, res);
     }
+  }
+
+  private doRemoteRequest(user: string, url: string, body, req: Request, res: Response<any>) {
+    const start = Date.now();
+    const apiTarget = new URL(apiUrl + url);
+
+    let request;
+    if (url.includes('/geo-convert')) {
+      request = req.pipe(httpRequest(
+        apiTarget.toString(),
+        {
+          headers: {
+            ...req.headers,
+            'Host': apiTarget.hostname,
+            'authorization': accessToken
+          }
+        }
+      ));
+    } else {
+      request = httpRequest[req.method.toLowerCase()](
+        apiTarget.toString(),
+        {
+          headers: {
+            ...req.headers,
+            'Host': apiTarget.hostname,
+            'content-length': typeof body === 'string' ? Buffer.byteLength(body) : req.headers['content-length'],
+            'authorization': accessToken
+          },
+          ...(['GET', 'DELETE'].includes(req.method) ? {} : {body})
+        }
+      );
+    }
+
+    request.on('end', function () {
+      LoggerService.info({
+        user,
+        action: LOG_SUCCESS,
+        request: {
+          method: req.method,
+          url,
+          body
+        },
+        response: {
+          statusCode: res.statusCode,
+        },
+        took: (Date.now() - start),
+        remote: req.connection.remoteAddress || '',
+      });
+    }).pipe(res);
   }
 }
